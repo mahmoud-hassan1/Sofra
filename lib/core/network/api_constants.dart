29dec0a3beea51fbccd2a2b478c50c7c4bfcfce7
MohamedEssam2127 <msam01619@gmail.com>
2026-06-28 00:42:39 +0300

    Fix merge conflict

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl="http://10.0.2.2:5000/api";
+  static const String baseUrl="http://10.0.2.2:5000/api/";
   static const String login = '/auth/login';
   static const String signup = '/auth/signup';
 }
